79cc52723cdd733190d20a8cf5c57b6e91a62f6e
Kevin Mader <kevinmader@gmail.com>
2015-02-17 00:29:34 +0100

    cleaning up repository, moving images to central location, creating introduction slides, adding template back

---
+++ common/schedule.R
@@ -0,0 +1,14 @@
+out.str<-"- 19th February - Introduction and Workflows
+- 26th February - Filtering and Image Enhancement (A. Kaestner)
+- 5th March - Basic Segmentation, Discrete Binary Structures
+- 12th March - Advanced Segmentation
+- 19th March - Applying Machine Learning
+- 27th March - Analyzing Single Objects
+- 2nd AprilAnalyzing Complex Objects
+- 9th April  Many Objects and Distributions
+- Statistics and Reproducibility
+- 17th April - Dynamic Experiments
+- 8th May - Big Data
+- 15th May - Guest Lecture - In-Operando Imaging of Batteries (V. Wood)
+- 22th May - Project Presentations"
+cat(out.str)
