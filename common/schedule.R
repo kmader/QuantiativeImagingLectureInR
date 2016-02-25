73a457207d0e9caa864485788124eeb061b0cb86
kmader <kevinmader@4quant.com>
2016-02-25 09:12:29 +0100

    first lecture slides updated

--- common/schedule.R
+++ common/schedule.R
@@ -1,5 +1,4 @@
 schedule.str<-"- 25th February - Introduction and Workflows
-- 26th February - Image Enhancement (A. Kaestner)
 - 3rd March - Image Enhancement (A. Kaestner)
 - 10th March - Ground Truth and Visualization
 - 17th March - Basic Segmentation, Discrete Binary Structures
@@ -11,15 +10,15 @@ schedule.str<-"- 25th February - Introduction and Workflows
 - 30th April - Dynamic Experiments
 - 12th May - Scaling Up / Big Data
 - 19th May - Guest Lecture - Applying Image Processing
-- 26th May - Guest Lecture - Applications Graphical Models and Machine Learning (A. Lucchi)
+- 26th May - Guest Lecture - Machine Learning / Deep Learning and More Advanced Approaches
 - 2nd June - Project Presentations"
 course.desc<-
   data.frame(Lecture=substring(gsub("\\s+"," ",strsplit(schedule.str,"\n")[[1]]),3),
              Description=c("Basic overview of the course, introduction to the basics of images and their acquisition, the importance of reproducibility and why workflows make sense for image processing",
                            "Overview of what techniques are available for assessing and improving the quality of images, specifically various filters, when to apply them, their side-effects, and how to apply them correctly",
+                           "Learning about ground-truth, validation, simulation and the visualization as they are related to image analysis",
                            "How to convert images into structures, starting with very basic techniques like threshold and exploring several automated techniques",
                            "More advanced techniques for extracting structures including basic clustering and classification approaches, and component labeling",
-                           "Applying more advanced techniques from the field of Machine Learning to image processing segmentation and analysis like Support vector machines (SVM) and Markov Random Fields (MRF)",
                            "The analysis and characterization of single structures/objects after they have been segmented, including shape and orientation",
                            "What techniques are available to analyze more complicated objects with poorly defined 'shape' using Distance maps, Thickness maps, and Voronoi tesselation",
                            "Extracting meaningful information for a collection of objects like their spatial distribution, alignment, connectivity, and relative positioning",
@@ -27,20 +26,22 @@ course.desc<-
                            "Performing tracking and registration in dynamic, changing systems covering object and image based methods",
                            "Performing large scale analyses on clusters and cloud-based machines and an introduction of how to work with 'big data' frameworks",
                            "Application of the course material to an actual scientific project from material science where we reproduce the results of a publication",
+                           "Applying more advanced techniques from the field of Machine Learning to image processing segmentation and analysis like Support vector machines (SVM) and Markov Random Fields (MRF)",
                            "The presentations of the student projects done in the class"
                            ),
              Applications=c(
                "Calculating the intensity for a folder full of images",
                "Removing detector noise from neutron images to distinguish different materials",
+               "Measuring improvement in signal to noise, validating methods",
                "Identify cells from noise, background, and dust",
                "Identifying fat and ice crystals in ice cream images",
-               "Training an algorithm to automatically identify cells",
                "Count cells and determine their average shape and volume",
                "Seperate clumps of cells, analyze vessel networks, trabecular bone, and other similar structures",
                "Quantify cells as being evenly spaced or tightly clustered or organized in sheets",
                "Determine if/how different a cancerous cell is from a healthly cell properly",
                "Turning a video of foam flow into metrics like speed, average deformation, and reorganization",
                "Performing large scale analyses using ETHs clusters and Amazons Cloud Resources, how to do anything with a terabytes of data",
+               "Training an algorithm to automatically identify cells",
                "",
                ""
                )
